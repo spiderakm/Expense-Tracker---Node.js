f08ea1e24c764ba7367e46f446c8a0cfbf0233cb
ashok kumar <spider.akm@gmail.com>
2023-03-05 08:34:56 +0530

    setup of express app

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const bodyParser=require("body-parser")
+const cors=require("cors")
+
+
+app.use(express.static("public"))
+app.use(cors())
+app.use(bodyParser.json())
+
+app.use(routes)
+
+sequelize.sync().then(() => {
+    app.listen(4000)
+})
+
