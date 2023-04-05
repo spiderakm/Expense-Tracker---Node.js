1cccd85d7a84f489a23ce761eba7639b456dd607
ashok kumar <spider.akm@gmail.com>
2023-04-05 09:30:36 +0530

    Optimize the SQL query for leaderboard using left outer joint

--- controllers/premium.js
+++ controllers/premium.js
@@ -1,27 +1,27 @@
 const User = require('../models/userModel')
 const Expense = require('../models/expenseModel')
 const sequelize = require('../utils/db')
-const { use } = require('../routes/user')
+
 
 exports.getLeaderBoard = async (req,res) => {
     try {
-        const users = await User.findAll({
-            attributes: ['id','name']
-        })
-        const userAggrigatedExpenses = await Expense.findAll({
-            attributes: ['UserId',[sequelize.fn('sum',sequelize.col('expenses.amount')),'totalCost']],
-            group: ['UserId']
-        })
-
-        
-        var userLeaderBoardDetails = []
-        users.forEach((user) => {
-            userLeaderBoardDetails.push({name:user.name,totalCost : userAggrigatedExpenses[user.id]})
+        const userAggrigatedExpenses = await User.findAll({
+            attributes:["id","name",[sequelize.fn("sum",sequelize.col("expenseDetails.amount")),"totalCost"]],
+            include: [
+                {
+                    model: Expense,
+                    attributes: []
+                }
+            ],
+            group: ['User.id'],
+            order: [['totalCost', 'DESC']]
         })
 
 
-        console.log(userLeaderBoardDetails);
-        res.status(200).json(userLeaderBoardDetails)
+
+
+        console.log(userAggrigatedExpenses);
+        res.status(200).json(userAggrigatedExpenses)
         
 
 
